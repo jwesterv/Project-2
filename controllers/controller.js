9e4f15bb8c1d9d76f6671c415b8998cfe126d9f4
myrean <dhughes1@uci.edu>
2019-03-06 20:07:39 -0800

    json route

--- controllers/controller.js
+++ controllers/controller.js
@@ -10,15 +10,13 @@ router.get("/", function (req, res) {
 
   Farms.findAll({}).then(function (results) {
 
-    res.render("index", {
-      farms: results
-    });
+    res.json(results);
   });
 
   //create a burger, push to mysql, redirect to / to show new burger
   router.post("/api/create", function (req, res) {
 
-    Burger.create(
+    Farms.create(
       {
         burger_name: req.body.burgerText,
         devoured: 0
