aa85cffce34dbc33477fd77111b81136f609736c
myrean <dhughes1@uci.edu>
2019-03-09 10:44:05 -0800

    shows farm img

--- public/js/consumer.js
+++ public/js/consumer.js
@@ -12,7 +12,12 @@ let zipcode = url.replace("?q=", '');
   .then(
     function(res) {
       $.get("/api/farms/" + zipcode,function(data){
-        console.log(data)
+        console.log(data[0].img_src)
+        Img = $("<img>");
+            Img.addClass("img-fluid");
+            Img.attr("src", data[0].img_src);
+            Img.attr("width", 200);
+ $("#farmImg").append(Img);
         $("#farmReturn").append("<h1>" + data[0].farm_name + "</h1>" + "<p>" + data[0].zipcode + "</p>"+ "<p>" + data[0].id + "</p>");
       // location.assign("/api/farms/"+ zipcode)
       })
