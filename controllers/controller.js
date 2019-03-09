3ac0c1a98efa53615378ede6c27ebe77e395d320
myrean <dhughes1@uci.edu>
2019-03-09 01:42:21 -0800

    added additional routes but not pulling all data

--- controllers/controller.js
+++ controllers/controller.js
@@ -7,74 +7,37 @@ var db = require("../models");
 
 //Main
 //========================
-router.get("/api/farms", function (req, res) {
-    console.log(res);
-    farm.findAll({})
-        .then(function (results) {
-            console.log(results);
-            res.json(results);
+router.get("/api/farms/:zipcode", function (req, res) {
+    console.log(req.params)
+    if (req.params.farm) {
+
+        db.farm.findAll({
+            where: {
+                zipcode: req.params.zipcode
+            }
+        }).then(function (result) {
+            return res.json(result);
+        });
+    } else {
+        db.farm.findAll({}).then(function (result) {
+            return res.json(result);
+        });
+    }
+});
+
+router.get("/api/farms/", function (req, res) {
+    // console.log(res);
+    db.farm.findAll({})
+        .then(function (dbGet) {
+            console.log(dbGet);
+            res.json(dbGet);
         });
 
-
-  
-
-    //create a farm, push to mysql, redirect to / consumer
-    // router.post("/api/create", function (req, res) {
-
-    //     Farm.create(
-    //         {
-    //             farm_name: req.body.farmNameText,
-    //             img_src = req.body.imageText,
-    //             zipcode = req.body.zipcodeText,
-    //             description = req.body.descriptionText
-
-    //         }).then(function () {
-    //             res.redirect("/consumer");
-    //         })
-
-    // });
-
-    // router.post("/api/create", function (req, res) {
-
-    //     FormData.create(
-    //         {
-    //             first_name: ,
-    //             last_name: ,
-    //             website: ,
-    //             description: ,
-    //             phone: ,
-    //             email: ,
-    //             farm_name: ,
-    //             farm_address: ,
-    //             city: ,
-    //             state: ,
-    //             zipcode: ,
-    //             deliver: ,
-    //             option: 
-
-    //         }).then(function () {
-    //             res.redirect("/consumer");
-    //         })
-
-    // });
-
-    //update the db to devoured true
-    router.post("/api/update/:id", function (req, res) {
-        farm.update({
-            where: {
-                id: req.params.id
-            }
-        }).then(function (results) {
-            res.end();
-        })
-
-    });
-
-
 })
+
 router.post("/api/posts/:zipcode", function (req, res) {
     console.log(req.params.zipcode);
-  
+
 
     db.farm.findAll({
         where: {
@@ -85,5 +48,62 @@ router.post("/api/posts/:zipcode", function (req, res) {
         res.json(dbPost);
     });
 });
+//create a farm, push to mysql, redirect to / consumer
+// router.post("/api/create", function (req, res) {
+
+//     Farm.create(
+//         {
+//             farm_name: req.body.farmNameText,
+//             img_src = req.body.imageText,
+//             zipcode = req.body.zipcodeText,
+//             description = req.body.descriptionText
+
+//         }).then(function () {
+//             res.redirect("/consumer");
+//         })
+
+// });
+
+// router.post("/api/create", function (req, res) {
+
+//     FormData.create(
+//         {
+//             first_name: ,
+//             last_name: ,
+//             website: ,
+//             description: ,
+//             phone: ,
+//             email: ,
+//             farm_name: ,
+//             farm_address: ,
+//             city: ,
+//             state: ,
+//             zipcode: ,
+//             deliver: ,
+//             option: 
+
+//         }).then(function () {
+//             res.redirect("/consumer");
+//         })
+
+// });
+
+
+// router.post("/api/update/:id", function (req, res) {
+//     farm.update({
+//         where: {
+//             id: req.params.id
+//         }
+//     }).then(function (results) {
+//         res.end();
+//     })
+
+// });
+
+
+
+
+
+
 //export the routes / router
 module.exports = router;
